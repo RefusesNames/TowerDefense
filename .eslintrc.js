fc5edf348013f29b2eaa9e2aa3f210413ba1a450
Jerome Reinlaender <jerome.reinlaneder@gmail.com>
2020-04-04 19:18:58 +0200

    Add eslint support for typescript

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es6": true
+	},
+	"extends": [
+		"airbnb-base",
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parserOptions": {
+		"ecmaVersion": 2018,
+		"parser": "@typescript-eslint/parser",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"ignorePatterns": [
+		"node_modules/",
+		"dist/"
+	],
+	"rules": {
+	}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,40 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-	entry: './typescript/main.ts',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	resolve: {
-		extensions: ['.ts', '.js', 'scss']
-	},
-	module: {
-		rules: [
-			{ 
-				test: /\.ts$/,
-				use: ['ts-loader'],
-				exclude: /node_modules/
-			},
-			{ 
-				test: /\.scss$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'sass-loader'
-				]
-			}
-		]
-	},
-	devtool: 'eval-source-map',
-	plugins: [
-		new CopyWebpackPlugin([
-			{
-				from: 'html/index.html',
-			}
-		]),
-		new MiniCssExtractPlugin({
-			filename: 'style.css'
-		})
-	]
+  entry: './typescript/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js', 'scss'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: 'html/index.html',
+      },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
 };
