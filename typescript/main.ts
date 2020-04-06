92968886a591cf8a56632fac01c22c4e576efa00
Jerome Reinlaender <jerome.reinlaneder@gmail.com>
2020-04-06 22:47:17 +0200

    Add scene, properly respond to resize

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,4 +1,30 @@
 import '../scss/style.scss';
+
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate(): void {
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+}
+animate();
+
+window.addEventListener('resize', (): void => {
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+});
