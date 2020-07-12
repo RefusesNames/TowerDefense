11e171bbe6943200fb70291f6e1defbb094e7167
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2020-07-12 17:56:41 +0200

    Add neighborhoods

--- typescript/pathfinding/pathfinding.ts
+++ typescript/pathfinding/pathfinding.ts
@@ -8,9 +8,42 @@ export interface PathFindingAlgorithm{
 };
 
 export abstract class Neighborhood {
-
+	abstract getNeighbors(mid: Coordinate): Array<Coordinate>;
 };
 
+export class VonNeumannNeighborhood extends Neighborhood {
+
+	getNeighbors(mid: Coordinate): Array<Coordinate> {
+		return [
+			{ x: mid.x - 1, y: mid.y },
+			{ x: mid.x + 1, y: mid.y },
+			{ x: mid.x, y: mid.y - 1 },
+			{ x: mid.x, y: mid.y + 1},
+		];
+	}
+
+}
+
+export class MooreNeighborhood extends Neighborhood {
+
+	getNeighbors(mid: Coordinate): Array<Coordinate> {
+		return [
+			{ x: mid.x - 1, y: mid.y - 1 },
+			{ x: mid.x - 1, y: mid.y },
+			{ x: mid.x - 1, y: mid.y + 1 },
+			{ x: mid.x, y: mid.y - 1 },
+			{ x: mid.x, y: mid.y + 1 },
+			{ x: mid.x + 1, y: mid.y - 1 },
+			{ x: mid.x + 1, y: mid.y },
+			{ x: mid.x + 1, y: mid.y + 1 },
+		];
+	}
+
+}
+
 export interface Map {
-	getNeighborhood(mid: Coordinate): Array<Coordinate>;
+	getNeighborhood(mid: Coordinate, neighborhood: Neighborhood): Array<Coordinate>;
+	setObstacle(coordinate: Coordinate, obstacle: boolean): void;
+	setFree(coordinate: Coordinate, free: boolean): void;
+	isFree(coordinate: Coordinate): boolean;
 };
