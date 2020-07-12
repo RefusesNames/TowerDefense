6609d6844c06434c0da5277e71b1bbce5f3ce5f5
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2020-07-12 22:39:00 +0200

    Add trivial obstacle map

--- typescript/pathfinding/pathfinding.ts
+++ typescript/pathfinding/pathfinding.ts
@@ -47,3 +47,43 @@ export interface Map {
 	setFree(coordinate: Coordinate, free: boolean): void;
 	isFree(coordinate: Coordinate): boolean;
 };
+
+export class ArrayMap implements Map {
+
+	private fieldIsFree: boolean[][];
+
+	constructor(width: number, height: number) {
+		this.fieldIsFree = [];
+		for(let x = 0; x < width; ++x) {
+			let column: boolean[] = [];
+			for(let y = 0; y < height; ++y) {
+				column.push(true);
+			}
+			this.fieldIsFree.push(column);
+		}
+	}
+
+	getNeighborhood(mid: Coordinate, neighborhood: Neighborhood): Array<Coordinate> {
+		return neighborhood
+			.getNeighbors(mid)
+			.filter( (coordinate) => 
+				coordinate.x >= 0 &&
+				coordinate.x < this.fieldIsFree.length &&
+				coordinate.y >= 0 &&
+				coordinate.y < this.fieldIsFree[0].length
+			 );
+	}
+
+	setObstacle(coordinate: Coordinate, obstacle: boolean): void {
+		this.fieldIsFree[coordinate.x][coordinate.y] = !obstacle;
+	}
+
+	setFree(coordinate: Coordinate, free: boolean): void {
+		this.fieldIsFree[coordinate.x][coordinate.y] = free;
+	}
+
+	isFree(coordinate: Coordinate): boolean {
+		return this.fieldIsFree[coordinate.x][coordinate.y];
+	}
+
+};
