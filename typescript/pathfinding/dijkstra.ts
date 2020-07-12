5dd0c1d738835c67128a4acf12aee9b92ff45197
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2020-07-12 21:16:07 +0200

    Pass neighborhood to dijkstra

--- typescript/pathfinding/dijkstra.ts
+++ typescript/pathfinding/dijkstra.ts
@@ -1,9 +1,16 @@
 import {
 	PathFindingAlgorithm,
-	Coordinate
+	Coordinate,
+	Neighborhood
 } from './pathfinding';
 
 export class Dijkstra implements PathFindingAlgorithm {
+
+	private neighborhood : Neighborhood;
+
+	constructor(neighborhood: Neighborhood) {
+		this.neighborhood = neighborhood;
+	}
 
 	getPath(start: Coordinate, end: Coordinate): Array<Coordinate> {
 		return [];
